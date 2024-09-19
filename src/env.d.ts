ed8220aad81789a14210db2a29f4122d05fd4a8f
6lueparr0t <6lueparr0t@gmail.com>
2024-09-19 15:41:02 +0900

    feat: contentful 삭제

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,5 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly CONTENTFUL_SPACE_ID: string;
-  readonly CONTENTFUL_DELIVERY_TOKEN: string;
-  readonly CONTENTFUL_PREVIEW_TOKEN: string;
+  // readonly DOT_ENV_VARIABLE: string;
 }
